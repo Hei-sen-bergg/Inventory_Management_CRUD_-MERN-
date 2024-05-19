a93c9c564b0f8f78608be642324efc02ecec3732
Hei-sen-bergg <senrajshekar98@gmail.com>
2024-05-19 23:20:48 +0530

    First push, basic frontend

---
+++ Frontend/src/components/InsertProduct.js
@@ -0,0 +1,96 @@
+import React,{useState} from "react";
+import { Await, NavLink, useNavigate } from 'react-router-dom';
+
+const InsertProduct=()=>{
+    const[productName,setProductName] = useState("")
+    const[productPrice,setProductPrice] = useState()
+    const [productBarcode, setProductBarcode] = useState(); 
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+    const navigate = useNavigate("");
+
+    const setName = (e)=>{
+        setProductName(e.target.value);
+    }
+    const setPrice = (e)=>{
+        setProductPrice(e.target.value);
+    }
+    const setBarcode = (e) => {
+        const value = e.target.value.slice(0, 12);
+        setProductBarcode(value);
+    };
+
+    const addProduct = async (e)=> {
+
+        if(!productName || !productPrice || !productBarcode){
+            setError("All fields are required.")
+            return;
+        }
+
+        setLoading(true)
+        setError("")
+
+        try{
+            const res = await fetch("http://localhost:3001/insertproduct", {
+                method:"POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({"ProductName": productName, "ProductPrice": productPrice, "ProductBarcode": productBarcode })
+            })
+
+            await res.json()
+
+            if(res.status ===201){
+                alert("Data Inserted")
+                setProductName("")
+                setProductPrice(0)
+                setProductBarcode(0)
+                navigate('/products')
+            }
+            else if  (res.status === 442){
+                alert("Product is already added with that barcode.")
+            }
+            else{
+                setError("Something went wrong. PLease try again")
+            }
+        
+        }
+        catch(err){
+            setError("An error occurred. Please try again later.");
+            console.error(err);
+        }finally{
+            setLoading(false)
+        }
+    }
+
+     return(
+        <div className="container-fluid p-5">
+            <h1 className=''>Enter Product Information</h1>
+            <div className="mt-5 col-lg-6 col-md-6 col-12 fs-4">
+            <label htmlFor="product_name" className="form-label fw-bold">Product Name</label>
+                <input type="text" onChange={setName} value={productName} className="form-control fs-5" id="product_name" placeholder="Enter Product Name" required />
+            </div>
+            <div className="mt-3 col-lg-6 col-md-6 col-12 fs-4">
+                <label htmlFor="product_price" className="form-label fw-bold">Product Price</label>
+                <input type="number" onChange={setPrice} value={productPrice} className="form-control fs-5" id="product_price" placeholder="Enter Product Price" required />
+            </div>
+            <div className="mt-3 mb-5 col-lg-6 col-md-6 col-12 fs-4">
+                <label htmlFor="product_barcode" className="form-label fw-bold">Product Barcode</label>
+                <input type="number" onChange={setBarcode} value={productBarcode} maxLength={12} className="form-control fs-5" id="product_barcode" placeholder="Enter Product Barcode" required />
+            </div>
+            <div className='d-flex justify-content-center col-lg-6 col-md-6'>
+                <NavLink to="/products" className='btn btn-primary me-5 fs-4'>Cancel</NavLink>
+                <button type="submit" onClick={addProduct} className="btn btn-primary fs-4" disabled={loading}>{loading ? 'Inserting...' : 'Insert'}</button>
+            </div>
+            <div className="col text-center col-lg-6">
+                {error && <div className="text-danger mt-3 fs-5 fw-bold">{error}</div>}
+            </div>
+
+        </div>
+     )
+
+}
+
+
+export default InsertProduct
